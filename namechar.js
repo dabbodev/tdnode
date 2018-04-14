740aa09f1ddd1b28a895c584922f4e604726ec8d
David Giles <dg1423@hotmail.com>
2018-04-14 11:49:56 -0700

    test

--- namechar.js
+++ namechar.js
@@ -25,7 +25,7 @@ var NameChar = function () {
         properties: ['read', 'write']
     });
     this.ddata = new ddata();
-    this.payload = [];
+    this.payload = [{'text': "Please connect to New Display"}];
 };
 
 util.inherits(NameChar, BlenoCharacteristic);
